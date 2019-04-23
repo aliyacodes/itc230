9a32ce15cc0b7b16116ee3fa3a3205b1f5bb288c
aliyacodes <aliyaasken@gmail.com>
2019-04-22 18:30:34 -0700

    Got things to respond, but not the way we want yet

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-    
+
+
 const http = require("http");
-  http.createServer((req,res) => {
+http.createServer((req,res) => {
     console.log("Create server");
     const path = req.url.toLowerCase();
-      switch(path) {
+    switch(path) {
         case '/':
-        const fs = require("fs");
-        fs.readFile("public/home.html", (err, data) =>{
-          if (err) return console.error(err);
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end(data.toString());
-        });
-          break;
-         case '/about':
+            var fs = require("fs");
+            fs.readFile("public/home.html", (err, data) =>{
+                if (err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            });
+            break;
+        case '/about':
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('About page');
             break;
-          
+
+        case '/get':
+            var fs = require("fs");
+            fs.readFile("lib/cartoon.js", (err, data) =>{
+                if (err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(data.toString());
+            });
+            break;
+
         default:
-          res.writeHead(404, {'Content-Type': 'text/plain'});
-          res.end('Not found');
-          break;
-        }
- 
-  }).listen(process.env.PORT || 3000);
-    console.log("Server created");
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+            break;
+    }
+
+}).listen(process.env.PORT || 3000);
+console.log("Server created");
