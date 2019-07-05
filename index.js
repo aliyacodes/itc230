20697dd14428c4ceb174fe3ff6ffd944f211e689
aliyacodes <aasken01@seattlecentral.edu>
2019-07-05 13:45:21 -0700

    add details route

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+
 let cartoon = require("lib/cartoon.js");
 
 const express = require("express");
@@ -8,46 +10,59 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // allows direct navigation to static files
-app.use(require("body-parser").urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-let handlebars =  require("express-handlebars");
-app.engine(".html", handlebars({extname: '.html'}));
+const handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
 app.set("view engine", ".html");
 
 // HOME - send static file as response
-app.get('/', function(req,res){
-    res.type('text/html');
-    res.sendFile(__dirname + '/public/home.html');
+// app.get('/', (req,res) => {
+//     console.log(req.query)
+//     res.type('text/html');
+//     res.sendFile(__dirname + '/public/home.html');
+// });
+
+app.get('/', (req,res) => {
+    res.render('home', {name: req.query.name});
 });
 
 // ABOUT - send plain text response
-app.get('/about', function(req,res){
+app.get('/about', (req,res) => {
     res.type('text/plain');
-    res.send('About page');
+    res.send('About Page');
+    //res.send('About ' + req.query.name + '\'s page');
 });
 
-// SEARCH - handle POST
-app.post('/get', function(req,res){
+
+
+
+// SEARCH - handle POST (post renders body)
+app.post('/details', (req,res) => {
     console.log(req.body);
-    let header = 'Searching for: ' + req.body.show + '<br>';
     let found = cartoon.get(req.body.show);
     res.render("details", {show: req.body.show, result: found});
 });
 
-// DELETE - handle GET
-app.get('/delete', function(req,res){
+// DELETE - handle GET (get renders query)
+app.get('/delete', (req,res) => {
     let result = cartoon.delete(req.query.show); // delete cartoon object
     res.render('delete', {show: req.query.show, result: result});
 });
 
+// app.post('/add', (req,res) => {
+//     let found = cartoon.add(req.body.show); // add cartoon object
+//     res.render('add', {show: req.body.show, result: found});
+// });
+
 // define 404 handler
-app.use(function(req,res) {
+app.use((req,res) => {
     res.type('text/plain');
     res.status(404);
     res.send('404 - Not found');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Express started');
+app.listen(app.get('port'), () => {
+    console.log('Express started at ' + __dirname);
 });
 
