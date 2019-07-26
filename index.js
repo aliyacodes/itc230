b8cc71c78d12b550147b8068396f3ef59d71097a
aliyacodes <aasken01@seattlecentral.edu>
2019-07-25 19:27:30 -0700

    HW6 REST API's

--- index.js
+++ index.js
@@ -18,18 +18,28 @@ app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
 app.set("view engine", ".html");
 
 
-// find all documents
+// const routes = require('./routes.js');
+// app.use('/routes', routes);
+app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header for api route
+
+
+/* NOTES */
+// GET renders query
+// POST renders BODY
+
+
+
+// FIND ALL DOCUMENTS
 app.get('/', (req,res) => {
 
     Cartoon.find({}, (err, cartoons) => {
         if (err) {
             console.log(err);
         } else {
-            res.render('home', {cartoons: cartoons });
+            res.render('home', {cartoons: cartoons});
         }
     });
 });
-
 
 // ABOUT
 app.get('/about', (req,res) => {
@@ -37,27 +47,17 @@ app.get('/about', (req,res) => {
     res.send('About Page');
 });
 
-// DELETE    -    (get renders query)
+// DELETE
 app.get('/delete', (req,res) => {
     Cartoon.deleteOne({ show: req.query.show }, (err, deleted) => {
         if (err) return next(err);
         Cartoon.countDocuments((err, total) => {
-            res.render('delete', {show: req.query.show, total: total, deleted: deleted } );
+            res.render('delete', {show: req.query.show, total: total, deleted: deleted} );
         });
     });
 });
 
-
-// ADD    -    (post renders body)
-app.post('/add', (req,res,next) => {
-    const newCartoon = {show: req.body.show, network: req.body.network, airdate: req.body.airdate };
-    Cartoon.updateOne({show: req.body.show}, newCartoon, {upsert: true}, (err, result) => {
-        if (err) return next(err);
-        res.render('details', {result: newCartoon, action: 'added'} );
-
-    });
-});
-
+// CARTOON LIST LINK TO INFO
 app.get('/details', (req,res,next) => {
     Cartoon.findOne({ show: req.query.show }, (err, cartoon) => {
         if (err) return next(err);
@@ -65,14 +65,73 @@ app.get('/details', (req,res,next) => {
     });
 });
 
+// ADD    -    (post renders body)
+app.post('/add', (req,res,next) => {
+    const newCartoon = { show: req.body.show, network: req.body.network, airdate: req.body.airdate };
+    Cartoon.updateOne({ show: req.body.show }, newCartoon, {upsert: true}, (err, result) => {
+        if (err) return next(err);
+        res.render('details', {result: newCartoon, action: 'added'} );
 
-// SEARCH
+    });
+});
+
+// SEARCH BAR
 app.post('/details', (req,res,next) => {
     Cartoon.findOne({ show: req.body.show }, (err, cartoon) => {
         if (err) return next(err);
         res.render('details', {result: cartoon} );
     });
 });
+
+/* API ROUTES */
+
+// FIND ALL DOCUMENTS - API
+app.get('/api/details', (req,res,next) => {
+    Cartoon.find({}, (err, cartoons) => {
+        if (err) return next(err);
+        res.json(cartoons);
+    });
+});
+
+// DELETE - API
+app.get('/api/delete/:show', (req,res,next) => {
+    Cartoon.deleteOne({ show: req.params.show }, (err, deleted) => {
+        if (err) return next(err);
+        Cartoon.countDocuments((err, total) => {
+            res.json(deleted);
+        });
+    });
+});
+
+// CARTOON LIST LINK TO INFO - API
+app.get('/api/details/:show', (req,res,next) => {
+    Cartoon.findOne({ show: req.params.show }, (err, cartoon) => {
+        if (err) return next(err);
+        res.json(cartoon);
+    });
+});
+
+// ADD - API
+app.post('/api/add/:show/:network/:airdate', (req,res,next) => {
+    const newCartoon = { show: req.params.show, network: req.params.network, airdate: req.params.airdate };
+    Cartoon.updateOne({ show: req.params.show }, newCartoon, {upsert: true}, (err, result) => {
+        if (err) return next(err);
+        res.json(result);
+
+    });
+});
+
+// SEARCH BAR - API
+app.post('/api/details/:show', (req,res,next) => {
+    Cartoon.findOne({ show: req.params.show }, (err, cartoon) => {
+        if (err) return next(err);
+        res.json(cartoon);
+    });
+});
+
+/* END API ROUTES */
+
+
 
 // define 404 handler
 app.use((req,res) => {
@@ -81,6 +140,7 @@ app.use((req,res) => {
     res.send('404 - Not found');
 });
 
+// START APP
 app.listen(app.get('port'), () => {
     console.log('Express started at ' + __dirname);
 });
