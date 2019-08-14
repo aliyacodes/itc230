b5245937f1d4675a33329ee55b32eb4f100f310e
aliyacodes <aasken01@seattlecentral.edu>
2019-08-13 20:08:46 -0700

    HW 8 React CRUD

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/views')); // allows direct navigation to static files
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 const handlebars =  require("express-handlebars");
@@ -29,9 +30,7 @@ app.get('/', (req,res) => {
             console.log(err);
         }
             console.log(cartoons);
-            res.render('home_demo', {cartoons: JSON.stringify(cartoons)});
-            //res.render('home', {cartoons: cartoons});
-
+            res.render('home2', {cartoons: JSON.stringify(cartoons)});
     });
 });
 
@@ -50,49 +49,50 @@ app.get('/about', (req,res) => {
     res.send('About Page');
 });
 
-// ADD
+// ADD - API
 app.post('/api/add/', (req,res, next) => {
     // find & update existing item, or add new
-    if (!req.body.show) { // insert new document
+    if (!req.body._id) { // insert new document
         let cartoon = new Cartoon({show:req.body.show, network:req.body.network, airdate:req.body.airdate});
         cartoon.save((err,newCartoon) => {
             if (err) return next(err);
-            //console.log(newCartoon)
-            res.json({updated: 0, show: newCartoon.show});
+            console.log(newCartoon);
+            res.json({updated: 0, _id: newCartoon._id});
         });
     } else { // update existing document
         Cartoon.updateOne({show: req.body.show}, {
-            show:req.body.show,
+            show: req.body.show,
             network: req.body.network,
             airdate: req.body.airdate }, (err, result) => {
             if (err) return next(err);
             res.json({updated: result.nModified, show: req.body.show});
+            console.log(result);
         });
     }
 });
 
 
-// ADD API
-app.get('/api/add/:show/:network/:airdate', (req,res, next) => {
-    // find & update existing item, or add new
-    let show = req.params.show;
-    Cartoon.update({ show: show}, {show: show, network: req.params.network, airdate: req.params.airdate },
-        {upsert: true }, (err, result) => {
-            if (err) return next(err);
-            // nModified = 0 for new item, = 1+ for updated item
-            res.json({updated: result.nModified});
-        });
-});
-
+// ADD - API
+// app.get('/api/add/:show/:network/:airdate', (req,res, next) => {
+//     // find & update existing item, or add new
+//     let show = req.params.show;
+//     Cartoon.update({show: show},{show: req.params.show, network: req.params.network, airdate: req.params.airdate },
+//         {upsert: true }, (err, result) => {
+//             if (err) return next(err);
+//             // nModified = 0 for new item, = 1+ for updated item
+//             res.json({updated: result.nModified});
+//             console.log(result);
+//         });
+// });
 
 
 // DELETE - API
-app.get('/api/delete/:show', (req,res,next) => {
-    Cartoon.deleteOne({ show: req.params.show }, (err, deleted) => {
+app.get('/api/delete/:id', (req,res,next) => {
+    Cartoon.deleteOne({ "_id": req.params.id }, (err, deleted) => {
         if (err) return next(err);
-        Cartoon.countDocuments((err, total) => {
-            res.json(deleted);
-        });
+             res.json({"deleted": deleted});
+            console.log(deleted);
+
     });
 });
 
@@ -112,14 +112,28 @@ app.get('/api/details/:show', (req,res,next) => {
     });
 });
 
+// app.get('/api/details/:id', (req,res,next) => {
+//     Cartoon.findOne({ "_id": req.params.id }, (err, cartoon) => {
+//         if (err) return next(err);
+//         res.json(cartoon);
+//     });
+// });
 
-// CARTOON LIST LINK TO INFO
+
+// CARTOON LIST
 app.get('/details', (req,res,next) => {
-    Cartoon.findOne({ show: req.query.show }, (err, cartoon) => {
+    Cartoon.findOne({ "_id": req.query.id }, (err, cartoon) => {
         if (err) return next(err);
         res.render('details', {result: cartoon} );
     });
 });
+
+// app.get('/details', (req,res,next) => {
+//     Cartoon.findOne({ show: req.query.show }, (err, cartoon) => {
+//         if (err) return next(err);
+//         res.render('details', {result: cartoon} );
+//     });
+// });
 
 
 
